1fca97b98fafe64abaa6af3a3154f60816c7be2e
Mishael Ogochukwu <49244132+mishael-o@users.noreply.github.com>
2023-02-05 14:08:05 +0200

    Build fix

--- src/Benchmark/SimpleSqlBuilder.BenchMark/Program.cs
+++ src/Benchmark/SimpleSqlBuilder.BenchMark/Program.cs
@@ -1,3 +1,5 @@
 ﻿using BenchmarkDotNet.Running;
 
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes. Will be removed when upgraded to .net 7.
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes
